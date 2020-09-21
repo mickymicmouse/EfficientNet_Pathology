9524e21a7419f683541a8db25548da2052fdef1d
seungjun <mickymicmouse@gmail.com>
2020-09-22 00:39:11 +0900

    transform

--- pytorch_version/main.py
+++ pytorch_version/main.py
@@ -9,8 +9,9 @@ import nsml
 from nsml.constants import DATASET_PATH, GPU_NUM
 import torch 
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader 
+from torch.utils.data import Dataset, DataLoader, ConcatDataset
 from sklearn.metrics import *
+import torchvision as tv
 
 ######################## DONOTCHANGE ###########################
 def bind_model(model):
@@ -67,26 +68,39 @@ def label_loader (root_path, keys):
 
 
 class PathDataset(Dataset): 
-    def __init__(self,image_path, labels=None, test_mode= True): 
+    def __init__(self,image_path, labels=None, test_mode= True, transform = None): 
         self.len = len(image_path)
         self.image_path = image_path
         self.labels = labels 
         self.mode = test_mode
+        self.transform = transform
 
     def __getitem__(self, index): 
         im = cv2.imread(self.image_path[index])
-        im = im.reshape(3,im.shape[0],im.shape[1])
-        
+        # numpy
+        # transforms = need PIL image
                 ### REQUIRED: PREPROCESSING ###
 
+        
+        
         if self.mode:
+            im = self.transform(im)
+            im = np.array(im)
+            im = im.reshape(3,im.shape[0],im.shape[1])
             return torch.tensor(im,dtype=torch.float32)
         else:
+            im = self.transform(im)
+            im = np.array(im)
+            im = im.reshape(3,im.shape[0],im.shape[1])
+            
             return torch.tensor(im,dtype=torch.float32),\
                  torch.tensor(self.labels[index] ,dtype=torch.long)
 
     def __len__(self): 
         return self.len
+
+    
+    
 def fmeasure(output, target):
     _, pred = output.topk(1, 1, True, True)
     pred = pred.view(-1,1)
@@ -105,6 +119,8 @@ def fmeasure(output, target):
     gt_len   =  target.data.long().sum()
 
     return TP,FP,TN,FN,pred_len, gt_len
+
+
 if __name__ == '__main__':
 
     ########## ENVIRONMENT SETUP ############
@@ -118,7 +134,7 @@ if __name__ == '__main__':
     ######################################################################
 
     # hyperparameters
-    args.add_argument('--epoch', type=int, default=1)
+    args.add_argument('--epoch', type=int, default=100)
     args.add_argument('--batch_size', type=int, default=64) 
     args.add_argument('--learning_rate', type=int, default=0.0001)
     args.add_argument('--train_ratio', type=int, default=0.8)
@@ -161,27 +177,67 @@ if __name__ == '__main__':
         valid_image = image_path[int(total_len*train_ratio):]
         valid_label = labels[int(total_len*train_ratio):]
         
-        
-        train_data = PathDataset(train_image, train_label, test_mode=False)
-        valid_data = PathDataset(valid_image, valid_label, test_mode=False)
-        train_loader = DataLoader(\
-            dataset=PathDataset(train_data, train_label, test_mode=False), 
-                batch_size=batch_size, shuffle=True)
-        valid_loader = DataLoader(\
-            dataset=PathDataset(valid_data, valid_label, test_mode=False), 
-                batch_size=batch_size, shuffle=True)
-                
+        train_transform1 = tv.transforms.Compose([
+                tv.transforms.ToPILImage(mode = 'RGB'),
+                tv.transforms.Resize(512),
+                tv.transforms.RandomAffine(0.1),
+                tv.transforms.RandomCrop(64),
 
+                ])
+    
+        # 회전
+        train_transform2 = tv.transforms.Compose([
+                tv.transforms.ToPILImage(mode = 'RGB'),
+                tv.transforms.Resize(512),
+                tv.transforms.RandomCrop(64),
+                tv.transforms.RandomHorizontalFlip(p=0.5),
+
+                ])
+        #수평 플립
+        train_transform3 = tv.transforms.Compose([
+                tv.transforms.ToPILImage(mode = 'RGB'),
+                tv.transforms.Resize(512),
+                tv.transforms.RandomCrop(64),
+                tv.transforms.RandomVerticalFlip(p=0.5)
+                ])
+        #수직 플립
+        train_transform4 = tv.transforms.Compose([
+                tv.transforms.ToPILImage(mode = 'RGB'),
+                tv.transforms.Resize(512),
+                tv.transforms.RandomCrop(64),
+                ])
+        # original
+        
+        test_transform = tv.transforms.Compose([
+                tv.transforms.ToPILImage(mode = 'RGB'),
+                tv.transforms.Resize(512),
+                tv.transforms.CenterCrop(64)
+                ])
+        
+        train_data1 = PathDataset(train_image, train_label, test_mode=False,transform = train_transform1)
+        train_data2 = PathDataset(train_image, train_label, test_mode=False,transform = train_transform2)
+        train_data3 = PathDataset(train_image, train_label, test_mode=False,transform = train_transform3)
+        train_data4 = PathDataset(train_image, train_label, test_mode=False,transform = train_transform4)
+        
+        
+        valid_data = PathDataset(valid_image, valid_label, test_mode=False,transform = test_transform)
+        train_loader = DataLoader(dataset=ConcatDataset(train_data1,train_data2,train_data3,train_data4), 
+                batch_size=batch_size, shuffle=True, drop_last = True)
+        valid_loader = DataLoader(dataset=valid_data, 
+                batch_size=batch_size, shuffle=True, drop_last = True)
+                
+        print('train number is : '+str(len(train_data1)))
+        print('valid number is : '+str(len(valid_data)))
         best=0 #for metric
         
         # Train the model
         for epoch in range(num_epochs):
             pred_sum = 0#model output
             gt_sum = 0#label
-            tp_sum=0
-            fp_sum=0
-            fn_sum=0
-            tn_sum=0
+            tp_sum=0.00001
+            fp_sum=0.00001
+            fn_sum=0.00001
+            tn_sum=0.00001
             acc=0
             total=0
             for i, (images, labels) in enumerate(train_loader):
@@ -224,13 +280,15 @@ if __name__ == '__main__':
             
             
             #metric 통합
-            f1=f1 = (2*precision*sens / (precision + sens)) * 100
+            
             accuracy=acc/total
             sens=tp_sum/(tp_sum+fn_sum)
             spec=tn_sum/(tn_sum+fp_sum)
             prec=tp_sum/(tp_sum+fp_sum)
             npv=tn_sum/(tn_sum+fn_sum)
+            f1= (2*prec*sens / (prec + sens)) * 100
             total_metric=(f1+accuracy+sens+spec+prec+npv)/6
+            
             if best<total_metric:
                 best=total_metric
                 nsml.report(summary=True, step=epoch, epoch_total=num_epochs, loss=loss.item())#, acc=train_acc)
@@ -239,8 +297,10 @@ if __name__ == '__main__':
                 
 
             
-            nsml.report(summary=True, step=epoch, epoch_total=num_epochs, loss=loss.item())#, acc=train_acc)
+            nsml.report(summary=True, step=epoch, epoch_total=num_epochs, loss=loss.item(), acc = total_metric)#, acc=train_acc)
+            print(total_metric)
             nsml.save(epoch)
+            
 
             
         
